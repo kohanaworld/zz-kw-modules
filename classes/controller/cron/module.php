1fb3d20190b2cb05b8bc52cb88f078965e727d68
biakaveron <biakaveron@gmail.com>
2011-02-16 00:16:41 +0300

    added logging to Cron_Module controller

--- classes/controller/cron/module.php
+++ classes/controller/cron/module.php
@@ -20,6 +20,10 @@ class Controller_Cron_Module extends Controller_Cron {
 
 		$page = arr::get($status, 'page', 1);
 		$stop = arr::get($status, 'stopped', 0);
+		// a reason for stopping cron execution
+		// @TODO define constant values? something like Cron::END_NORMAL or Cron::END_TIMELIMIT
+		$reason = 'end of search';
+		$level = 'info';
 
 		while ( ! $stop)
 		{
@@ -31,17 +35,34 @@ class Controller_Cron_Module extends Controller_Cron {
 					// limit execution time for ~4 minutes
 					echo 'time limit!'.PHP_EOL;
 					$stop = TRUE;
+					$reason = 'end of time limit';
+					$level = 'notice';
 				}
 			}
 			catch(phpGitHubApiRequestException $e)
 			{
 				echo 'github exception ('.$e->getMessage().'), code#'.$e->getCode().PHP_EOL;
 				$stop = TRUE;
+				$reason = 'github limitation';
+				$level = 'notice';
 			}
 
 			$crawler->update_status($page, $stop);
 			echo 'page #'.$page.' scanned'.PHP_EOL;
 			$page ++;
+		}
+
+		try {
+			Jelly::factory('log')->set(array(
+				'executant'    => 'crawler_module',
+				'action'       => 'search',
+				'text'         => 'search complete ('.$reason.')',
+				'level'        => $level,
+			))->save();
+		}
+		catch( Validation_Exception $e)
+		{
+			Kohana::$log->add('error', 'unable to save cron log: '.$e->getMessage());
 		}
 	}
 
